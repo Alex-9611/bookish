6d9e0c7d2a0fda6076d03b15a44e87b27d28696a
Alex-9611 <Alex.Worthington@softwire.com>
2022-10-27 09:15:56 +0100

    Began adding a frontend to a login page that will eventually produce and return a signed JWT

--- bookish.js
+++ bookish.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { queryForBooks } from "./bookishQueries.js";
+import jwt from "jsonwebtoken";
+import path from "path";
 
 const app = express();
 const port = 3000;
+
+// app.use(express.static('frontend'));
 
 app.get("/", (req, res) => {
     const books = queryForBooks().then((output) => {
@@ -12,6 +16,12 @@ app.get("/", (req, res) => {
     // turn into a list Books (the class)
 
     // then return
+});
+app.get("/loginPage", (req, res) => {
+    res.sendFile('/Work/Bootcamp/bookish' + '/frontend/index.html');
+    // request username and passaword
+    // provide and return a jwt
+    const token = jwt.sign({username : '_'}, 'secret');
 });
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
